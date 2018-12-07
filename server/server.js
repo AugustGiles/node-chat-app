e96809a6ad5f13c34156ca26c16dd19094815a38
August Giles <augustgiles8@gmail.com>
2018-12-06 16:55:17 -0800

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const path = require('path');
-const express = require('express')
+// http is another native node api - needed w/ socket.io
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 
 // path is a native node api that condenses paths
 // here we get the pathcoming to this file, we back out, then enter public
 // using path, this public path just goes straight to public!
-const publicPath = path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
-let app = express()
+let app = express();
+let server = http.createServer(app);
+// this creates the web socket server
+let io = socketIO(server)
 
 // middlewares
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Started on Port ${port}`))
+// event listener for the io
+io.on('connection', (socket) => {
+  console.log('new user connected')
+
+  socket.on('disconnect', () => {
+    console.log('user was disconnected')
+  })
+})
+
+server.listen(port, () => console.log(`Started on Port ${port}`));
